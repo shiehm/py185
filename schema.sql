cbfa69f22df6ce66cf5feb4df4a0f859f3ff17ae
Melvin Shieh <shieh.melvin@gmail.com>
2025-11-04 15:32:50 -0500

    pre-class refactoring

--- schema.sql
+++ schema.sql
@@ -6,3 +6,9 @@ CREATE TABLE expenses (
     memo text NOT NULL,
     created_on date NOT NULL DEFAULT CAST(NOW() AS DATE)
 );
+
+DELETE FROM expenses;
+INSERT INTO expenses (amount, memo, created_on)
+       VALUES (14.56, 'Pencils', NOW()),
+              (3.29, 'Coffee', NOW()),
+              (49.99, 'Text Editor', NOW());
